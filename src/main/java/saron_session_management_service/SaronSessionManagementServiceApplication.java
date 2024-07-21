4da1782d0295dc93058212f78667ac80d5b4fa6a
sarontadesse <sarontadesse456@gmail.com>
2024-07-20 22:42:58 -0500

    initial commit

---
+++ src/main/java/saron_session_management_service/SaronSessionManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package saron_session_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaronSessionManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaronSessionManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saron_session_management_service/SaronSessionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package saron_session_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaronSessionManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
